644ab5a29b7b0797b01e1a1e57d4fb47e95f6cda
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-10-01 17:47:28 +0100

    add traingle corner to the open menu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 window.onload = () => {
   const iconMenu = document.querySelector("#icon-menu");
   const navLinks = document.querySelector("#nav-links");
+  const triangle = document.querySelector("#triangle");
 
   iconMenu.addEventListener("click", () => {
     navLinks.classList.toggle("hidden");
+    triangle.classList.toggle("hidden");
   });
 };
